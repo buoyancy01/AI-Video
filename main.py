8415caba1cf7df0a7f075c1ff548eb2e0e667f94
Buoyancy <oluwaseyidavid2005@gmail.com>
2025-06-18 05:47:50 +0100

    Update main.py for open port

--- main.py
+++ main.py
@@ -70,4 +70,4 @@ def generate():
 
 # Optional: Add a main block to run the Flask app if this file is executed directly
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
